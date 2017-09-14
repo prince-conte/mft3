5304d138633e4475ab9fef477554d6fbc591a4ef
prince-conte <prince-conte@outlook.com>
2017-09-14 21:53:54 +0300

    ou

--- app/assets/javascripts/components/editor.js
+++ app/assets/javascripts/components/editor.js
@@ -2,13 +2,21 @@
 
 $(function() {
 
+    var icons = Quill.import('ui/icons');
+    icons['header'] = 'T';
+    icons['bold'] = '<i class="mdi mdi-format-bold">';
+    icons['italic'] = '<i class="mdi mdi-format-italic">';
+    icons['link'] = '<i class="mdi mdi-link-variant">';
+    icons['blockquote'] = '<i class="mdi mdi-format-quote-close">';
+    icons['bullet'] = '<i class="mdi mdi-format-list-bulleted">';
+
     if ($('#editor-container').length) {
 
         var quill = new Quill('#editor-container', {
             modules: {
 
                 toolbar: [
-                    ['bold', 'italic','link', { 'header': 1 }, { 'header': 2 }, 'blockquote', { 'list': 'bullet'}]
+                    ['bold', 'italic','link', { 'header': 2 }, { 'header': 3 }, 'blockquote', { 'list': 'bullet'}]
                 ]
             },
 
