cc58f118fe47eb933a079b5ac1ec716a1031db2a
Alexander Kozlov <alexabderl@coalla.ru>
2017-09-24 15:22:54 +0300

    edits buttons

--- app/assets/javascripts/components/editor.js
+++ app/assets/javascripts/components/editor.js
@@ -1,127 +1,47 @@
 'use strict';
 
+$(function() {
 
+    var icons = Quill.import('ui/icons');
+    icons['header'] = '<span>T</span>';
+    icons['bold'] = '<i class="mdi mdi-format-bold">';
+    icons['italic'] = '<i class="mdi mdi-format-italic">';
+    icons['link'] = '<i class="mdi mdi-link-variant">';
+    icons['blockquote'] = '<i class="mdi mdi-format-quote-close">';
+    icons['list'] = '<i class="mdi mdi-format-list-bulleted">';
 
+    if ($('#editor-container').length) {
 
-var editor = new MediumEditor('.editable', {
-    delay: 1000,
-    targetBlank: true,
-    toolbar: {
-        buttons: [
+        var quill = new Quill('#editor-container', {
+            modules: {
 
-            {
-                name: 'bold',
-                tagNames: ['i'],
-                contentDefault: '<i class="mdi mdi-format-bold"></i>',
+                toolbar: [
+                    ['bold', 'italic','link', { 'header': 2 }, { 'header': 3 }, 'blockquote', { 'list': 'bullet'}]
+                ]
             },
 
-            {
-            name: 'italic',
-            tagNames: ['i'],
-            contentDefault: '<i class="mdi mdi-format-italic"></i>',
-            },
-                {
-                name: 'anchor',
-                contentDefault: '<i class="mdi mdi-link-variant"></i>',
-            },
-
-            {
-                name: 'h2',
-                action: 'append-h2',
-                aria: 'header type 2',
-                tagNames: ['h2'],
-                contentDefault: '<span>T</span>',
-                classList: ['custom-class-h2'],
-                attrs: {
-                    'data-custom-attr': 'attr-value-h2'
-                }
-            },
-
-            {
-                name: 'h3',
-                action: 'append-h3',
-                aria: 'header type 2',
-                tagNames: ['h3'],
-                contentDefault: '<span class="min">T</span>',
-                classList: ['custom-class-h3'],
-                attrs: {
-                    'data-custom-attr': 'attr-value-h3'
-                }
-            },
-
-            {
-                name: 'quote',
-                contentDefault: '<b><i class="mdi mdi-format-quote-close"></i></b>',
-            },
-
-            {
-                name: 'unorderedlist',
-                contentDefault: '<b><i class="mdi mdi-format-list-bulleted"></i></b>',
-            }],
-
-        diffLeft: 25,
-        diffTop: 10,
-    },
-
-    anchor: {
-        placeholderText: 'Type a link',
-        customClassOption: 'btn',
-        customClassOptionText: 'Create Button'
-    },
-
-    anchorPreview: {
-        hideDelay: 300
-    },
-
-    placeholder: {
-        /* This example includes the default options for placeholder,
-           if nothing is passed this is what it used */
-        text: 'Type your text',
-        hideOnClick: true
-    }
-});
-
-    $(function () {
-        $('.editable').mediumInsert({
-            editor: editor,
-            addons: {
-                images: {
-                    fileUploadOptions: {
-                        url: 'upload.php'
-                    }
-                }
-            }
+            theme: 'bubble'   // Specify theme in configuration
         });
 
-});
+        $('#show-controls').click(function() {
+            $(this).toggleClass('active');
+            quill.focus();
+        });
+
+        quill.on('text-change', function(delta, source) {
 
 
+            if (source == 'api') {
+                console.log("An API call triggered this change.");
+            } else if (source == 'user') {
+                console.log("A user action triggered this change.");
+            }
 
-$(function(){
+        });
 
-    var fixedBox = $('.js-fixed-box');
-    var fixedBoxOffset = fixedBox.offset();
-
-    $(window).scroll(function() {
-
-        if (($(this).scrollTop() >= fixedBoxOffset.top)) {
-
-            fixedBox.css('left', fixedBoxOffset.left);
-            fixedBox.css('width', fixedBox.outerWidth());
-            fixedBox.addClass('is-fixed')
-        }
-        else {
-
-            fixedBox.removeClass('is-fixed')
-            fixedBox.css('left', '');
-            fixedBox.css('width', '');
-        }
-
-    });
-
+    }
 
 
 
 
 });
-
