ac41530ab94731b9a84ef033d8af4ea3728b706b
Alexander Kozlov <alexabderl@coalla.ru>
2017-09-26 21:53:58 +0300

    edits buttons

--- app/assets/javascripts/components/editor.js
+++ app/assets/javascripts/components/editor.js
@@ -16,101 +16,57 @@ $(function() {
         let Block = Quill.import('blots/block');
         let BlockEmbed = Quill.import('blots/block/embed');
 
-        class BoldBlot extends Inline { }
-        BoldBlot.blotName = 'bold';
-        BoldBlot.tagName = 'strong';
 
-        class ItalicBlot extends Inline { }
-        ItalicBlot.blotName = 'italic';
-        ItalicBlot.tagName = 'em';
 
-        class LinkBlot extends Inline {
-            static create(url) {
-                let node = super.create();
-                node.setAttribute('href', url);
-                node.setAttribute('target', '_blank');
-                return node;
-            }
-
-            static formats(node) {
-                return node.getAttribute('href');
-            }
-        }
-        LinkBlot.blotName = 'link';
-        LinkBlot.tagName = 'a';
-
-        class BlockquoteBlot extends Block { }
-        BlockquoteBlot.blotName = 'blockquote';
-        BlockquoteBlot.tagName = 'blockquote';
-
-        class HeaderBlot extends Block {
-            static formats(node) {
-                return HeaderBlot.tagName.indexOf(node.tagName) + 1;
-            }
-        }
-        HeaderBlot.blotName = 'header';
-        HeaderBlot.tagName = ['H1', 'H2'];
-
-        class DividerBlot extends BlockEmbed { }
-        DividerBlot.blotName = 'divider';
-        DividerBlot.tagName = 'hr';
 
         class ImageBlot extends BlockEmbed {
             static create(value) {
-                let node = super.create();
-                node.setAttribute('alt', value.alt);
-                node.setAttribute('src', value.url);
+                let node = super.create(value);
+                let img = document.createElement('img');
+                let figcaption = document.createElement('figcaption');
+
+                img.setAttribute('src', value);
+                node.appendChild(img);
+                node.appendChild(figcaption);
+                figcaption.setAttribute('data-figcaption', ficaptionImage);
                 return node;
             }
 
-            static value(node) {
-                return {
-                    alt: node.getAttribute('alt'),
-                    url: node.getAttribute('src')
-                };
+            static value(domNode) {
+                return domNode.firstChild.getAttribute('src');
             }
         }
         ImageBlot.blotName = 'image';
+        ImageBlot.className = 'ql-image';
         ImageBlot.tagName = 'figure';
 
+
+
+
         class VideoBlot extends BlockEmbed {
-            static create(url) {
-                let node = super.create();
-                node.setAttribute('src', url);
-                node.setAttribute('frameborder', '0');
-                node.setAttribute('allowfullscreen', true);
+            static create(value) {
+                let node = super.create(value);
+                let iframe = document.createElement('iframe');
+                iframe.setAttribute('frameborder', '0');
+                iframe.setAttribute('allowfullscreen', true);
+                iframe.setAttribute('src', value);
+                node.appendChild(iframe);
                 return node;
             }
 
-            static formats(node) {
-                let format = {};
-                if (node.hasAttribute('height')) {
-                    format.height = node.getAttribute('height');
-                }
-                if (node.hasAttribute('width')) {
-                    format.width = node.getAttribute('width');
-                }
-                return format;
-            }
-
-            static value(node) {
-                return node.getAttribute('src');
-            }
-
-            format(name, value) {
-                if (name === 'height' || name === 'width') {
-                    if (value) {
-                        this.domNode.setAttribute(name, value);
-                    } else {
-                        this.domNode.removeAttribute(name, value);
-                    }
-                } else {
-                    super.format(name, value);
-                }
+            static value(domNode) {
+                return domNode.firstChild.getAttribute('src');
             }
         }
         VideoBlot.blotName = 'video';
-        VideoBlot.tagName = 'iframe';
+        VideoBlot.className = 'ql-video';
+        VideoBlot.tagName = 'div';
+
+
+
+
+
+
 
         class TweetBlot extends BlockEmbed {
             static create(id) {
@@ -128,17 +84,18 @@ $(function() {
         TweetBlot.tagName = 'div';
         TweetBlot.className = 'tweet';
 
-        Quill.register(BoldBlot);
-        Quill.register(ItalicBlot);
-        Quill.register(LinkBlot);
-        Quill.register(BlockquoteBlot);
-        Quill.register(HeaderBlot);
-        Quill.register(DividerBlot);
+
         Quill.register(ImageBlot);
-        Quill.register(TweetBlot);
         Quill.register(VideoBlot);
 
-        let quill = new Quill('#editor-container');
+        var optionsEditor = {
+            placeholder: 'Текст или фото...',
+            theme: 'bubble'
+        };
+
+        let quill = new Quill('#editor-container', optionsEditor);
+
+
 
         quill.on(Quill.events.EDITOR_CHANGE, function(eventType, range) {
             if (eventType !== Quill.events.SELECTION_CHANGE) return;
@@ -167,62 +124,21 @@ $(function() {
             }
         });
 
-        $('#bold-button').click(function() {
-            quill.format('bold', true);
-        });
 
-        $('#italic-button').click(function() {
-            let active = $(this).hasClass('active');
-            quill.format('italic', true);
-        });
 
-        $('#link-button').click(function() {
-            let value = prompt('Enter link URL');
-            quill.format('link', value);
-        });
 
-        $('#blockquote-button').click(function() {
-            console.log('blockquote');
-            quill.format('blockquote', true);
-        });
 
-        $('#header-1-button').click(function() {
-            quill.format('header', 1);
-        });
+        // video add
 
-        $('#header-2-button').click(function() {
-            quill.format('header', 2);
-        });
 
-        $('#divider-button').click(function() {
-            let range = quill.getSelection(true);
-            quill.insertEmbed(range.index, 'divider', true, Quill.sources.USER);
-            quill.setSelection(range.index + 1, Quill.sources.SILENT);
-            $('#sidebar-controls').hide();
-        });
+        function addVideo() {
 
-        $('#image-button').click(function() {
 
             let range = quill.getSelection(true);
 
-            quill.insertEmbed(range.index, 'image', {
-                alt: 'Quill Cloud',
-                url: 'https://quilljs.com/0.20/assets/images/cloud.png'
-            }, Quill.sources.USER);
-            quill.setSelection(range.index + 1, Quill.sources.SILENT);
-            $('#sidebar-controls').hide();
-        });
+                var $v2;
 
-        $('#video-button').click(function() {
-
-            let range = quill.getSelection(true);
-            var link = prompt('Введите ссылку на видео', 100);
-
-
-            var $v1 = link,
-                $v2;
-
-// Для youtube
+            // Для youtube
             if($v1.indexOf('youtube.com') + 1 || $v1.indexOf('youtu.be') + 1) {
                 // Ищем и заменяем, чтобы проще обработать
                 $v2 = $v1.replace('.com/embed/', '.com/watch?v=');
@@ -236,7 +152,8 @@ $(function() {
 
                 $v2 = 'https://www.youtube.com/embed/' + $v2;
             }
-// Для Coub
+
+            // Для Coub
             else if ($v1.indexOf('http://coub.com/') + 1) {
 
                 // Производим замену
@@ -246,7 +163,8 @@ $(function() {
 
                 $v2 = $v2 + '?muted=false&amp;autostart=false&originalSize=false&hideTopBar=false&noSiteButtons=false&startWithHD=false';
             }
-// Для vimeo
+
+            // Для vimeo
             else if ($v1.indexOf('vimeo.com') + 1) {
                 // Производим замену
                 $v2 = $v1.replace('vimeo.com/', 'player.vimeo.com/video/');
@@ -266,15 +184,124 @@ $(function() {
 
 
 
-
-
-
             let url = $v2;
             quill.insertEmbed(range.index, 'video', url, Quill.sources.USER);
             quill.formatText(range.index + 1, 1, { height: '170', width: '100%' });
             quill.setSelection(range.index + 1, Quill.sources.SILENT);
             $('#sidebar-controls').hide();
+
+
+            $(document)
+                .find('.ql-editor iframe[src*="vimeo"], .ql-editor iframe[src*="youtube"]')
+                .each(function() {
+                    console.log('asd');
+                    $(this).wrap('<div class="content__video"/>');
+                });
+
+        }
+
+
+
+        var videoLink;
+        var $v1;
+
+        $(document).on('click touchend', '.js-add-link-video', function() {
+
+            videoLink = $(this).prev('input').val();
+
+            $v1  = videoLink;
+
+            $('.js-input-link-form').removeClass('is-active');
+
+            addVideo();
+
+            return false;
+
         });
+
+
+        $('#video-button').click(function() {
+
+            $('.js-input-link-form').addClass('is-active');
+
+
+
+        });
+
+        $('#image-button').click(function() {
+
+            $('.js-input-figcaption').addClass('is-active');
+
+
+
+        });
+
+
+        var ficaptionImage;
+
+        $(document).on('click touchend', '.js-add-figcaption', function() {
+
+            ficaptionImage = $(this).prev('input').val();
+
+
+            $('.js-input-figcaption').removeClass('is-active');
+
+            $('#picField').trigger('click');
+
+            return false;
+
+        });
+
+
+
+        var srcImage;
+
+        function addImage() {
+            let url = srcImage;
+
+            let range = quill.getSelection(true);
+
+            quill.insertEmbed(range.index, 'image', url, Quill.sources.USER);
+            quill.formatText(range.index + 1, 1);
+            quill.setSelection(range.index + 1, Quill.sources.SILENT);
+            $('#sidebar-controls').hide();
+        }
+
+        document.getElementById('picField').onchange = function (evt) {
+            var tgt = evt.target || window.event.srcElement,
+                files = tgt.files;
+
+            // FileReader support
+            if (FileReader && files && files.length) {
+                var fr = new FileReader();
+                fr.onload = function () {
+                    srcImage = fr.result;
+                    addImage();
+                }
+                fr.readAsDataURL(files[0]);
+            }
+
+            // Not supported
+            else {
+                // fallback -- perhaps submit the input to an iframe and temporarily store
+                // them on the server until the user's session ends.
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         $('#tweet-button').click(function() {
             let range = quill.getSelection(true);
